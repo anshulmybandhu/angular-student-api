ee5fa70216c8345528eef044e8b1bea7b0734447
anshulmybandhu <130960096+anshulmybandhu@users.noreply.github.com>
2023-04-17 12:23:57 +0530

    Add files via upload

---
+++ Controllers/GendersController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Student_Portal_API.service;
+using Student_Portal_API.service.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Portal_API.Controllers
+{
+    [Route("api/Genders")]
+    [ApiController]
+    public class GendersController : ControllerBase
+    {
+        private readonly IGenderRepository _genderRepository;
+
+        public GendersController(IGenderRepository genderRepository)
+        {
+            _genderRepository = genderRepository;
+        }
+
+        [Route("[action]")]
+        //[HttpGet]
+        public async Task<IActionResult> GetAllGender()
+        {
+            
+            return Ok(await _genderRepository.GetGenderAsync());
+
+        }
+    }
+}
